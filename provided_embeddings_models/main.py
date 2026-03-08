fdfb2c709ec5242c042ace6cb239c6181d0cb0f5
bri-personal <runnerb2003@gmail.com>
2026-03-08 15:13:42 -0700

    refactor main calling script

--- provided_embeddings_models/main.py
+++ provided_embeddings_models/main.py
@@ -1,26 +1,41 @@
+from typing import Tuple
+
 from constants import *
 from provided_embeddings_models.preprocessing import split_embeddings, build_preprocessing_pipeline
 from util import *
 
 # load dataset into pandas from csv
 data = load_embeddings_data(YAMNET_FILENAME)
-# print(data.head())
+print(data.head())
+
+
+def get_age_group_split() -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    age_group_cleaned = clean_embeddings_for_age_group(data)
+    # print(age_group_cleaned.head())
+    return split_embeddings(age_group_cleaned, 'age_group', val_size=0.12, regression=False)
+
+
+def get_age_split() -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    age_cleaned = clean_embeddings_for_age(data)
+    # print(age_cleaned.head())
+    return split_embeddings(age_cleaned, 'age', val_size=0.12, regression=True)
+
+
+def get_sex_split() -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    sex_cleaned = clean_embeddings_for_sex(data)
+    # print(sex_cleaned.head())
+    return split_embeddings(sex_cleaned, 'sex', val_size=0.12, regression=False)
+
 
 # age group isolated
-# age_group_cleaned = clean_embeddings_for_age_group(data)
-# print(age_group_cleaned.head())
+train_features, val_features, test_features, train_labels, val_labels, test_labels = get_age_group_split()
 
 # age (continuous) isolated
-age_cleaned = clean_embeddings_for_age(data)
-# print(age_cleaned.head())
+# train_features, val_features, test_features, train_labels, val_labels, test_labels = get_age_split()
 
 # sex (M/F) isolated
-# sex_cleaned = clean_embeddings_for_sex(data)
-# print(sex_cleaned.head())
+# train_features, val_features, test_features, train_labels, val_labels, test_labels = get_sex_split()
 
-# train/val/test split
-(train_features, val_features, test_features,
- train_labels, val_labels, test_labels) = split_embeddings(age_cleaned, 'age', val_size=0.12, regression=True)
 print(f'Train features: {train_features.shape}')
 print(f'Train labels: {train_labels.shape}')
 print(f'Val features: {val_features.shape}')
